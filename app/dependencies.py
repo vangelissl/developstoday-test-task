0a52a5396c2910e412071db3ea5185ab14e9e784
Maria Stashenko <mahryia@gmail.com>
2026-06-12 18:10:06 +0300

    update dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,17 +1,35 @@
 from fastapi import Depends
 
 from app.repositories.project_repo import ProjectRepository
+from app.repositories.place_repo import PlaceRepository
+
 from app.services.project_service import ProjectService
+from app.services.place_service import PlaceService
+
+
 from .database import get_db, AsyncSession
 
 
 async def get_project_repo(
-		session: AsyncSession = Depends(get_db), 
+    session: AsyncSession = Depends(get_db),
 ) -> ProjectRepository:
-	return ProjectRepository(session)
+    return ProjectRepository(session)
 
 
 async def get_project_service(
-		project_repo: ProjectRepository = Depends(get_project_repo)
+    project_repo: ProjectRepository = Depends(get_project_repo)
 ) -> ProjectService:
-	return ProjectService(project_repo)
+    return ProjectService(project_repo)
+
+
+async def get_place_repo(
+    session: AsyncSession = Depends(get_db),
+) -> PlaceRepository:
+    return PlaceRepository(session)
+
+
+async def get_place_service(
+    place_repo: PlaceRepository = Depends(get_place_repo),
+    project_repo: ProjectRepository = Depends(get_project_repo)
+) -> PlaceService:
+    return PlaceService(place_repo, project_repo)
